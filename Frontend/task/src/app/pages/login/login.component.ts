89285417e7ff55424f03a4f47d3a086262e85658
thisaramax20 <thisarathejana20@gmail.com>
2025-04-05 13:42:27 +0530

    bug fix #19 workflow

--- Frontend/task/src/app/pages/login/login.component.ts
+++ Frontend/task/src/app/pages/login/login.component.ts
@@ -8,7 +8,7 @@ import {
 } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { AuthServiceService } from '../../service/auth-service.service';
-import swal from 'sweetalert2';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-login',
   imports: [ReactiveFormsModule, CommonModule, RouterLink],
@@ -45,7 +45,7 @@ export class LoginComponent {
         this.loginForm.reset();
         this.submitted = false;
         res.hasOwnProperty('token') && this.AuthService.setToken(res.token);
-        swal.fire({
+        Swal.fire({
           title: 'Login Successful',
           icon: 'success',
           showConfirmButton: false,
@@ -56,7 +56,7 @@ export class LoginComponent {
       error: (err) => {
         console.error('Login failed:', err);
         this.submitted = false;
-        swal.fire({
+        Swal.fire({
           title: 'Login Failed',
           icon: 'error',
           showConfirmButton: false,
