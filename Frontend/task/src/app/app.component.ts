8b3cc8b55569ae4707f82a5cd28c2a0608dc3d10
thisaramax20 <thisarathejana20@gmail.com>
2025-04-05 13:34:24 +0530

    bug fix #18 workflow

--- Frontend/task/src/app/app.component.ts
+++ Frontend/task/src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
